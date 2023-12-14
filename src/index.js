8449c4d9de28b75ffea96b151c7b2f9baa7ea8b9
boburbekburkhonov <zafaroviccc@gmail.com>
2023-12-14 11:38:36 +0500

    feat: .

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import express from 'express'
-import { MongoClient } from 'mongodb';
-
-const mongoClient = new MongoClient('mongodb://127.0.0.1:27017/')
+import mongo from './utils/mongo.js'
 
 const app = express();
 
-(async () => {
-  try {
-     await mongoClient.connect();
-     app.locals.collection = mongoClient.db("usersdb").collection("users");
-     app.listen(3000);
-     console.log("Сервер ожидает подключения...");
- }catch(err) {
-     return console.log(err);
- }
-})();
+mongo()
+  .then(() => console.log('Connected'))
+  .catch((err) => console.error(err))
+
+
+app.listen(3000, console.log(3000));
